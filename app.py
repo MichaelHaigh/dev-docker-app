b9ca4a948221bd1426fad281c188b00f1f92c3f0
Michael Haigh <mahaigh@gmail.com>
2019-09-12 15:18:16 -0400

    Updated banner

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def main():
-    message = "Hello World!"
+    message = "Hello .DEV Audience!"
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
